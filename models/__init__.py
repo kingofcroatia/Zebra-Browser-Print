d6ac10d5893a9ba29e2bf7b1e047f2607eff2b87
kingofcroatia <auroradynamics95@gmail.com>
2025-11-19 15:37:27 +0000

    Delete models directory

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2025 Aurora Tech Group LTD, London, UK
-# Developer: Andrew
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
-
-from . import res_config_settings
